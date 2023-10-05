60a4bc88b2ce3db6290add884dbb5651beb29cfa
jdanelon <62486702+jdanelon@users.noreply.github.com>
2023-10-05 23:19:10 +0000

    rewritten some comments and added requirements.txt

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import numpy as np
 
+# reading bidimensional array from txt
 matrix = np.genfromtxt("matrix.txt", delimiter=',')
 print(f"Matriz original:\n{matrix}\n")
 
 # statistical measures from matrix
-print(f"Menor elemento por linha: {matrix.min(axis=1)}")
+print(f"Menor elemento de cada linha: {matrix.min(axis=1)}")
 print(f"Média dos elementos das mesmas colunas: {matrix.mean(axis=0)}")
 print(f"Soma de todos os elementos: {matrix.sum()}\n")
 
@@ -14,13 +15,12 @@ print(f"Matriz transposta:\n{transposed_matrix}\n")
 transposed_and_five = transposed_matrix + 5
 print(f"Matriz transposta + 5:\n{transposed_and_five}\n")
 
-# product between matrix and its transposed
-print(f"Matriz x Matriz transposta + 5:\n{matrix @ transposed_and_five}\n")
+# product between matrix and its transposed + 5
+print(f"Matriz x (Matriz transposta + 5):\n{matrix @ transposed_and_five}\n")
 
 # filter matrix
 filter = (matrix > 0) & (matrix < 20)
-print(
-    f"Os elementos positivos da matriz menores que 20 são: {matrix[filter]}\n")
+print(f"Os elementos positivos da matriz menores que 20 são: {matrix[filter]}\n")
 
 # arithmetic operation on matrix
 # identity + (2 * matrix * transposed_matrix)
